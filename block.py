af48a3556d50c9c4f855067bd165417c656ee12e
hidekido <lysenko-org@yandex.ru>
2016-12-07 15:30:47 +0300

    New libs and some changes

--- block.py
+++ block.py
@@ -1,13 +1,18 @@
 from pygame import *
 from pygame.locals import *
+import os
 
 PLATFORM_WIDTH = 25
 PLATFORM_HEIGHT = 25
 PLATFORM_COLOR = "#FF6262"
+DIR = os.path.dirname(__file__)
 
 class Platform(sprite.Sprite):
 	def __init__(self, x, y):
 		sprite.Sprite.__init__(self)
+		self.x = x
+		self.y = y
 		self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
 		self.image.fill(Color(PLATFORM_COLOR))
+		self.image = image.load("%s/blocks/platform.png" % DIR)
 		self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import *
 from block import *
+from player import *
+import os
 
 WINHEIGHT = 768
 WINWIDTH = 1024
@@ -10,7 +12,10 @@ pygame.init()
 screen = pygame.display.set_mode(DISPLAY)
 pygame.display.set_caption("Amazing Doctor Strange")
 bg = Surface((WINWIDTH,WINHEIGHT))
-
+bg.fill(Color(BACKGROUND_COLOR))
+entities = pygame.sprite.Group() # Все объекты
+platforms = [] # то, во что мы будем врезаться или опираться
+DIR = os.path.dirname(__file__)
 black = (0,0,0)
 
 def text_objects(text, font):
@@ -33,16 +38,45 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     screen.blit(textSurf, textRect)
 
 
+def lvlinit():
+    f = open("%s\\levels\\1.txt" % DIR, 'r')
+    level = []
+    line = f.readline()
+    while line != '':
+        line = line[1:-2]
+        level += [line]
+        line = f.readline()
+    x = y = 0 # координаты
+    for row in level: # вся строка
+        for col in row: # каждый символ
+            if col == "-":
+                pf = Platform(x,y)
+                entities.add(pf)
+                platforms.append(pf)
+            x += PLATFORM_WIDTH #блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT    #то же самое и с высотой
+        x = 0                   #на каждой новой строчке начинаем с нуля
+    
+    total_level_width  = len(level[0])*PLATFORM_WIDTH # Высчитываем фактическую ширину уровня
+    total_level_height = len(level)*PLATFORM_HEIGHT   # высоту
+    
+
 def main():
-	bg.fill(Color(BACKGROUND_COLOR))
-	mainLoop = True
-	while mainLoop:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				mainLoop = False
-		screen.blit(bg, (0,0)) 
-		pygame.display.update()
-	pygame.quit() 
+    hero = Player(55,55) # создаем героя по (x,y) координатам
+    left = right = False # по умолчанию - стоим
+    up = False
+    lvlinit()
+    timer = pygame.time.Clock()
+    mainLoop = True
+    while mainLoop:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                mainLoop = False
+        screen.blit(bg, (0,0))
+        for e in entities:
+            screen.blit(e.image, (e.x,e.y))
+        pygame.display.update()
+    pygame.quit() 
 
 if __name__ == '__main__':
 	main()
